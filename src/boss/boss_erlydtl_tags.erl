6580c44528469495856085f14bc2bc3245e9032e
Matt Husby <matthusby@gmail.com>
2012-01-02 13:40:23 -0600

    Add language to view rendering

--- src/boss/boss_erlydtl_tags.erl
+++ src/boss/boss_erlydtl_tags.erl
@@ -30,3 +30,19 @@ url(Variables, Options) ->
     RouterPid = boss_web:router_pid(list_to_atom(lists:concat([App]))),
     BaseURL = boss_web:base_url(list_to_atom(lists:concat([App]))),
     BaseURL ++ boss_router:unroute(RouterPid, Controller, Action, NoUndefinedVars).
+
+language(Variables, Options) ->
+    ListVars = lists:map(fun 
+            ({K, V}) when is_binary(V) -> {K, binary_to_list(V)}; 
+            ({K, V}) -> {K, V} 
+        end, Variables),
+    io:format("Vars: ~p~n", [Variables]),
+    io:format("Options: ~p~n", [Options]),
+    Value = proplists:get_value(string, ListVars),
+    io:format("String: ~p~n", [Value]),
+    case boss_db:find(language, [{name, 'equals', "string"}]) of
+        [Result] ->
+            Result;
+        [] ->
+            ok
+    end.
